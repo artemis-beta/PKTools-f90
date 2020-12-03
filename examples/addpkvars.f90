52c0229dbb610efa8f24fbc8274b1150a41f0540
Kristian Zarebski <krizar312@gmail.com>
2020-12-03 21:39:23 +0000

    Completed PKVar example

--- examples/addpkvars.f90
+++ examples/addpkvars.f90
@@ -1,12 +1,27 @@
 PROGRAM ADDPKVARS
     USE PKVAR_MODULE
     IMPLICIT NONE
-    TYPE(PKVAR) :: X, Y
+    TYPE(PKVAR) :: X, Y, Z
 
     X = PKVAR(1D2, 1D1)
     Y = PKVAR(56D0, 34D-2)
 
+    WRITE(*,*) "DEMONSTRATION OF PKVAR TYPE OF THE FORM (VALUE, ERROR):", &
+               NEW_LINE('A')
+
     WRITE(*,*) "VECTOR 1: ", X
-    WRITE(*,*) "VECTOR 2: ", Y
+    WRITE(*,*) "VECTOR 2: ", Y, NEW_LINE('A')
+
+    Z = X + Y
+    WRITE(*,*) "ADDITION: ", Z, NEW_LINE('A')
+
+    Z = X - Y
+    WRITE(*,*) "SUBTRACTION: ", Z, NEW_LINE('A')
+
+    Z = X * Y
+    WRITE(*,*) "MULTIPLICATION: ", Z, NEW_LINE('A')
+
+    Z = X / Y
+    WRITE(*,*) "DIVISION: ", Z, NEW_LINE('A')
 
 END PROGRAM ADDPKVARS
